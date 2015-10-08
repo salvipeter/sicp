375faa43809bb6468b415cb1e4d5d48d5f85e9ea
Peter Salvi <vukung@yahoo.com>
2015-10-08 18:57:12 +0900

    All code of 5.5.1-4. Compiles, but does not work yet.

--- sicp.lisp
+++ sicp.lisp
@@ -12694,7 +12694,308 @@ Some function calls are updated to the versions in the exercises."
 ;;; Exercise 5.30 END
 
 
-;;; Section 5.5
+;;; Section 5.5.1
+
+(defun compile% (exp target linkage)
+  (cond ((self-evaluating-p exp)
+         (compile-self-evaluating exp target linkage))
+        ((quotedp exp) (compile-quoted exp target linkage))
+        ((variablep exp) (compile-variable exp target linkage))
+        ((assignmentp exp) (compile-assignment exp target linkage))
+        ((definitionp exp) (compile-definition exp target linkage))
+        ((ifp exp) (compile-if exp target linkage))
+        ((lambdap exp) (compile-lambda exp target linkage))
+        ((beginp exp)
+         (compile-sequence (begin-actions exp) target linkage))
+        ((condp exp) (compile% (cond->if exp) target linkage))
+        ((applicationp exp) (compile-application exp target linkage))
+        (t (error "Unknown expression type: ~a -- COMPILE%" exp))))
+
+(defun make-instruction-sequence (needs modifies statements)
+  (list needs modifies statements))
+
+(defparameter +empty-instruction-sequence+
+  (make-instruction-sequence '() '() '()))
+
+
+;;; Section 5.5.2
+
+(defun compile-linkage (linkage)
+  (cond ((eq linkage 'return)
+         (make-instruction-sequence '(continue) '() '((goto (reg continue)))))
+        ((eq linkage 'next) (funcall +empty-instruction-sequence+))
+        (t (make-instruction-sequence '() '() `((goto (label ,linkage)))))))
+
+(defun end-with-linkage (linkage instruction-sequence)
+  (preserving '(continue) instruction-sequence (compile-linkage linkage)))
+
+(defun compile-self-evaluating (exp target linkage)
+  (end-with-linkage
+   linkage
+   (make-instruction-sequence
+    '() (list target)
+    `((assign ,target (const ,exp))))))
+
+(defun compile-quoted (exp target linkage)
+  (end-with-linkage
+   linkage
+   (make-instruction-sequence
+    '() (list target)
+    `((assign ,target (const ,(text-of-quotation exp)))))))
+
+(defun compile-variable (exp target linkage)
+  (end-with-linkage
+   linkage
+   (make-instruction-sequence
+    '(env) (list target)
+    `((assign ,target (op lookup-variable-value) (const ,exp) (reg env))))))
+
+(defun compile-assignment (exp target linkage)
+  (let ((var (assignment-variable exp))
+        (get-value-code (compile% (assignment-value exp) 'val 'next)))
+    (end-with-linkage
+     linkage
+     (preserving '(env) get-value-code
+                 (make-instruction-sequence
+                  '(env val) (list target)
+                  `((perform (op set-variable-value!) (const ,var) (reg val) (reg env))
+                    (assign ,target (const ok))))))))
+
+(defun compile-definition (exp target linkage)
+  (let ((var (definition-variable exp))
+        (get-value-code (compile% (definition-value exp) 'val 'next)))
+    (end-with-linkage
+     linkage
+     (preserving '(env) get-value-code
+                 (make-instruction-sequence
+                  '(env val) (list target)
+                  `((perform (op define-variable!) (const ,var) (reg val) (reg env))
+                    (assign ,target (const ok))))))))
+
+(defparameter *label-counter* 0)
+
+(defun new-label-number () (incf *label-counter*))
+
+(defun make-label (name)
+  (intern (format nil "~:@(~a~)~a" name (new-label-number))))
+
+(defun compile-if (exp target linkage)
+  (let ((t-branch (make-label 'true-branch))
+        (f-branch (make-label 'false-branch))
+        (after-if (make-label 'after-if)))
+    (let ((consequent-linkage
+           (if (eq linkage 'next) after-if linkage)))
+      (let ((p-code (compile% (if-predicate exp) 'val 'next))
+            (c-code (compile% (if-consequent exp) target consequent-linkage))
+            (a-code (compile% (if-alternative exp) target linkage)))
+        (preserving '(env continue) p-code
+                    (append-instruction-sequences
+                     (make-instruction-sequence
+                      '(val) '()
+                      `((test (op false?) (reg val)) (branch (label ,f-branch))))
+                     (parallel-instruction-sequences
+                      (append-instruction-sequences t-branch c-code)
+                      (append-instruction-sequences f-branch a-code))
+                     after-if))))))
+
+(defun compile-sequence (seq target linkage)
+  (if (last-exp-p seq)
+      (compile% (first-exp seq) target linkage)
+      (preserving '(env continue)
+                  (compile% (first-exp seq) target 'next)
+                  (compile-sequence (rest-exps seq) target linkage))))
+
+(defun compile-lambda (exp target linkage)
+  (let ((proc-entry (make-label 'entry))
+        (after-lambda (make-label 'after-lambda)))
+    (let ((lambda-linkage (if (eq linkage 'next) after-lambda linkage)))
+      (append-instruction-sequences
+       (tack-on-instruction-sequence
+        (end-with-linkage
+         lambda-linkage
+         (make-instruction-sequence
+          '(env) (list target)
+          `((assign ,target (op make-compiled-procedure) (label ,proc-entry) (reg env)))))
+        (compile-lambda-body exp proc-entry))
+       after-lambda))))
+
+(defun compile-lambda-body (exp proc-entry)
+  (let ((formals (lambda-parameters exp)))
+    (append-instruction-sequences
+     (make-instruction-sequence
+      '(env proc argl) '(env)
+      `(,proc-entry
+        (assign env (op compiled-procedure-env) (reg proc))
+        (assign env (op extend-environment) (const ,formals) (reg argl) (reg env))))
+     (compile-sequence (lambda-body exp) 'val 'return))))
+
+
+;;; Section 5.5.3
+
+(defun compile-application (exp target linkage)
+  (let ((proc-code (compile% (operator exp) 'proc 'next))
+        (operand-codes (mapcar (lambda (operand) (compile% operand 'val 'next))
+                               (operands exp))))
+    (preserving '(env continue) proc-code
+                (preserving '(proc continue) (construct-arglist operand-codes)
+                            (compile-procedure-call target linkage)))))
+
+(defun construct-arglist (operand-codes)
+  (let ((operand-codes (reverse operand-codes)))
+    (if (null operand-codes)
+        (make-instruction-sequence '() '(argl) '((assign argl (const ()))))
+        (let ((code-to-get-last-arg
+               (append-instruction-sequences
+                (car operand-codes)
+                (make-instruction-sequence
+                 '(val) '(argl)
+                 '((assign argl (op list) (reg val)))))))
+          (if (null (cdr operand-codes))
+              code-to-get-last-arg
+              (preserving '(env) code-to-get-last-arg
+                          (code-to-get-rest-args (cdr operand-codes))))))))
+
+(defun code-to-get-rest-args (operand-codes)
+  (let ((code-for-next-arg
+         (preserving '(argl) (car operand-codes)
+                     (make-instruction-sequence
+                      '(val argl) '(argl)
+                      '((assign argl (op cons) (reg val) (reg argl)))))))
+    (if (null (cdr operand-codes))
+        code-for-next-arg
+        (preserving '(env) code-for-next-arg
+                    (code-to-get-rest-args (cdr operand-codes))))))
+
+(defun compile-procedure-call (target linkage)
+  (let ((primitive-branch (make-label 'primitive-branch))
+        (compiled-branch (make-label 'compiled-branch))
+        (after-call (make-label 'after-call)))
+    (let ((compiled-linkage (if (eq linkage 'next) after-call linkage)))
+      (append-instruction-sequences
+       (make-instruction-sequence
+        '(proc) '()
+        `((test (op primitive-procedure?) (reg proc)) (branch (label ,primitive-branch))))
+       (parallel-instruction-sequences
+        (append-instruction-sequences
+         compiled-branch
+         (compile-proc-appl target compiled-linkage))
+        (append-instruction-sequences
+         primitive-branch
+         (end-with-linkage
+          linkage
+          (make-instruction-sequence
+           '(proc argl) (list target)
+           `((assign ,target (op apply-primitive-procedure) (reg proc) (reg argl)))))))
+       after-call))))
+
+(defparameter +all-regs+ '(env proc val argl continue))
+
+(defun compile-proc-appl (target linkage)
+  (cond ((and (eq target 'val) (not (eq linkage 'return)))
+         (make-instruction-sequence
+          '(proc) +all-regs+
+          `((assign continue (label ,linkage))
+            (assign val (op compiled-procedure-entry) (reg proc))
+            (goto (reg val)))))
+        ((and (not (eq target 'val)) (not (eq linkage 'return)))
+         (let ((proc-return (make-label 'proc-return)))
+           (make-instruction-sequence
+            '(proc) +all-regs+
+            `((assign continue (label ,proc-return))
+              (assign val (op compiled-procedure-entry) (reg proc))
+              (goto (reg val))
+              ,proc-return
+              (assign ,target (reg val))
+              (goto (label ,linkage))))))
+        ((and (eq target 'val) (eq linkage 'return))
+         (make-instruction-sequence
+          '(proc continue) +all-regs+
+          '((assign val (op compiled-procedure-entry) (reg proc))
+            (goto (reg val)))))
+        ((and (not (eq target 'val)) (eq linkage 'return))
+         (error "return linkage, target not val: ~a -- COMPILE" target))))
+
+
+;;; Section 5.5.4
+
+(defun registers-needed (s)
+  (if (symbolp s) '() (car s)))
+
+(defun registers-modified (s)
+  (if (symbolp s) '() (cadr s)))
+
+(defun statements (s)
+  (if (symbolp s) (list s) (caddr s)))
+
+(defun needs-register-p (seq reg)
+    (member reg (registers-needed seq)))
+
+(defun modifies-register-p (seq reg)
+  (member reg (registers-modified seq)))
+
+(defun append-instruction-sequences (&rest seqs)
+  (labels ((append-2-sequences (seq1 seq2)
+             (make-instruction-sequence
+              (list-union (registers-needed seq1)
+                          (list-difference (registers-needed seq2)
+                                           (registers-modified seq1)))
+              (list-union (registers-modified seq1)
+                          (registers-modified seq2))
+              (append (statements seq1) (statements seq2))))
+           (append-seq-list (seqs)
+             (if (null seqs)
+                 (funcall +empty-instruction-sequence+)
+                 (append-2-sequences (car seqs)
+                                     (append-seq-list (cdr seqs))))))
+    (append-seq-list seqs)))
+
+(defun list-union (s1 s2)
+  (cond ((null s1) s2)
+        ((member (car s1) s2) (list-union (cdr s1) s2))
+        (t (cons (car s1) (list-union (cdr s1) s2)))))
+
+(defun list-difference (s1 s2)
+  (cond ((null s1) '())
+        ((member (car s1) s2) (list-difference (cdr s1) s2))
+        (t (cons (car s1)
+                 (list-difference (cdr s1) s2)))))
+
+(defun preserving (regs seq1 seq2)
+  (if (null regs)
+      (append-instruction-sequences seq1 seq2)
+      (let ((first-reg (car regs)))
+        (if (and (needs-register-p seq2 first-reg)
+                 (modifies-register-p seq1 first-reg))
+            (preserving (cdr regs)
+                        (make-instruction-sequence
+                         (list-union (list first-reg) (registers-needed seq1))
+                         (list-difference (registers-modified seq1) (list first-reg))
+                         (append `((save ,first-reg)) (statements seq1) `((restore ,first-reg))))
+                        seq2)
+            (preserving (cdr regs) seq1 seq2)))))
+
+(defun tack-on-instruction-sequence (seq body-seq)
+  (make-instruction-sequence
+   (registers-needed seq)
+   (registers-modified seq)
+   (append (statements seq) (statements body-seq))))
+
+(defun parallel-instruction-sequences (seq1 seq2)
+  (make-instruction-sequence
+   (list-union (registers-needed seq1) (registers-needed seq2))
+   (list-union (registers-modified seq1) (registers-modified seq2))
+   (append (statements seq1) (statements seq2))))
+
+
+;;; Section 5.5.5
+
+#+nil
+(compile%
+ '(define (factorial (n)
+    (if (= n 1)
+        1
+        (* (factorial (- n 1)) n))))
+ 'val 'next)
 
 
 ;;Local Variables:
